9205c2e0d1a391a269a810d5b94bd21e1eec9f5e
Mauricio Cardoso <mauricio.cds00@gmail.com>
2022-08-15 21:25:17 -0300

    datasource

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 import { app } from "./app";
+import "dotenv/config";
+import { AppDataSource } from "./data-source";
 
 const PORT = process.env.PORT || 3004;
 
-app.listen(PORT, () => console.info(`Server running on port: ${PORT}`));
+AppDataSource.initialize()
+  .then(() => {
+    console.info("Connected to Postgres database");
+  })
+  .then(() => {
+    app.listen(PORT, () => console.info(`Server running on port: ${PORT}`));
+  });
